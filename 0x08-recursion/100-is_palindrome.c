d3520f5d5d1211d5db7fd8091265d52c4f6dd443
Cindy Johnson <cindyjohnson745@gmail.com>
2023-10-26 11:22:19 +0300

    betty style

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,7 +5,7 @@ int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - checks if a string is a palindrome
- * @s: atring to reverse
+ * @s: string to reverse
  *
  * Return: 1 if it is, 0 it's not
  */
@@ -20,17 +20,18 @@ int is_palindrome(char *s)
  * _strlen_recursion - returns the length of a string
  * @s: string to calculate the length of
  *
- * Returns: length of the string
+ * Return: length of the string
  */
 int _strlen_recursion(char *s)
-{	
+{
 	if (*s == '\0')
 		return (0);
 	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- *  check_pal - checks the characters recursively for palindrome
+ * check_pal - checks the characters recursively for
+ * palindrome
  *  @s: string to check
  *  @i: iterator
  *  @len: length of the string
@@ -41,7 +42,7 @@ int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
 		return (0);
-	if (1 >= len)
+	if (len <= 1)
 		return (1);
 	return (check_pal(s, i + 1, len - 1));
 }
