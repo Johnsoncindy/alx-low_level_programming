e41382aff6d01ad7e5283cf3a19d8cd882ee351e
Cindy Johnson <cindyjohnson745@gmail.com>
2023-11-02 23:48:56 +0300

    0x0c malloc 4

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *p;
-    unsigned int s1_len;
-    unsigned int s2_len;
-    unsigned int concat_len;
+	char *p;
+	unsigned int s1_len;
+	unsigned int s2_len;
+	unsigned int concat_len;
 
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
 
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-    if (n >= s2_len)
-    {
-        n = s2_len;
-    }
-    concat_len = s1_len + n;
+	if (n >= s2_len)
+	{
+		n = s2_len;
+	}
+	concat_len = s1_len + n;
 
-    p = malloc(concat_len + 1);
+	p = malloc(concat_len + 1);
 
-    if (p == NULL)
-    {
-        return (NULL);
-    }
-    strcpy(p, s1);
-    strncat(p, s2, n);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(p, s1);
+	strncat(p, s2, n);
 
-    return (p);
+	return (p);
 }
