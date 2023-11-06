4730b6c958f4c0c5f3a46748ebe4232a3c6ddcfb
Cindy Johnson <cindyjohnson745@gmail.com>
2023-11-07 02:38:38 +0300

    structure_typedef 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a structure.
+ * @name: first member
+ * @age: second member
+ * @owner: last member
+ *
+ * Description: This defines a structure called dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - define a new type as the new name for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
