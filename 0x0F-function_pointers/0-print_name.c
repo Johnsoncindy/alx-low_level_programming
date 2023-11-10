67671cbd09a87fdcd6adc0f01bea7d29d24c789a
Cindy Johnson <cindyjohnson745@gmail.com>
2023-11-10 04:44:22 +0300

    task 1 F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: A pointer to a null-terminated string containing printed name
+ * @f: a function pointer pointing to a function capable of printing a char
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: the pointer array
+ * @size: The size of the array
+ * @action: Function pointer that points to a function which will be executed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
